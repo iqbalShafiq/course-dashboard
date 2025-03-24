28cd521a7a7c022739bd682767791cc8164db9ec
syafiq <iqbal.syafiq.rozaan@gmail.com>
2025-03-25 06:15:32 +0700

    Update database settings to use PostgreSQL with environment variable configuration

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,8 +85,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("PG_DATABASE", "course_dashboard"),
+        "USER": os.environ.get("PG_USER", "admin"),
+        "PASSWORD": os.environ.get("PG_PASSWORD", "password"),
+        "HOST": os.environ.get("PG_HOST", "localhost"),
+        "PORT": os.environ.get("PG_PORT", "5433"),
     }
 }
 
